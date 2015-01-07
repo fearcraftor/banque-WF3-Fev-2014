c509ae8e0fc6cdb506b46985348f61ea2f641ef0
fearcraftor <flelong@live.fr>
2015-01-07 21:40:27 +0100

    Suggest
    Solution cool mais pas trop

--- index.php
+++ index.php
@@ -1,45 +1,13 @@
-<?php 
-include 'header.php'; ?>
+<?php include("header.php"); 
 
- <h1>Effectuer un virement  </h1>
+?>
 
-<?php 
-	// Afin de simplifier l'exercice, on spéficie ici 3 comptes et 3 clients.
+	<div class="container">
 
-	$client1->afficherInformations();
-	$client1->compte->crediterCompte(100);
-	$client1->afficherInformations();
+		<h1>Bienvenue sur WF-Banque</h1>
+		<h3>La banque qui en a gros</h3>
 
-	if($client1->compte->debiterCompte(10000000000000))
-		$client1->afficherInformations();
-	else
-		echo "Tentative de depassement de votre decouvert <br>";
-	
-	$client1->afficherInformations();
+	</div>
 
 
-	$compte1->setDecouvert(12);
-	$compte1->afficherInformations();
-
- ?>
-
-
-
- <h2>Informations sur les comptes</h2>
-
-<?php 
-	$compte1->afficherInformations();
-	$compte2->afficherInformations();
-	$compte3->afficherInformations();
- ?>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php include("footer.php"); ?>
