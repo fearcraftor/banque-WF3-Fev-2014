8f1a41a1089c67e5ee31acfc9078c1f8ea4f93d6
fearcraftor <flelong@live.fr>
2015-01-07 21:39:33 +0100

    Suggest
    Solution cool mais pas trop

--- virement.php
+++ virement.php
@@ -1,51 +1,99 @@
-<?php 
-include 'header.php';
- ?>
+<?php include("header.php"); 
 
-  <h1>Effectuer un virement  </h1>
+include("donneesTest.php");
+
+?>
+
+<div class="container">
+
+	<div class="page-header">
+	
+		<h1>Effectuer un virement</h1>
+
+	</div>
+
+	<form method="post" action="virements.php" name="virement">
+	
+		<div class="row">
+
+			<div class="col-lg-4 col-sm-4 col-xs-4">
+
+				<label>Emetteur</label>
+				
+				<select type="text" name="emetteur" class="form-control">
+					<?php
+						$item = "all";
+
+						$item2 = "num";
+						
+						foreach ($clients as $value)
+						{
+							$solde = $value -> getCompte() -> getSolde();
+
+							echo "<option value='".$value -> getClient($item2)."'>".$value -> getClient($item)." ".number_format($solde, 2, ',', ' ')." €</option>";
+						}
+					?>
+				
+				</select>
+
+			</div>
+			
+			<div class="col-lg-4 col-md-4 col-xs-4">
+
+				<label>Destinataire</label>
+				
+				<select type="text" name="destinataire" class="form-control">
+					<?php
+						$item = "all";
+
+						$item2 = "num";
+
+						foreach ($clients as $value)
+						{
+							$solde = $value -> getCompte() -> getSolde();
+
+							echo "<option value='".$value -> getClient($item2)."'>".$value -> getClient($item)." ".number_format($solde, 2, ',', ' ')." €</option>";
+						}
+					?>
+				</select>
+
+			</div>
+			
+			<div class="col-lg-4 col-md-4 col-xs-4">
+
+				<label>Montant</label>
+				
+				<div class="input-group">
+					
+					<input name="somme" type="text" class="form-control" aria-label=" ">
+					<span class="input-group-addon" id="sizing-addon2">€</span>
+
+				</div>
+
+			</div>
 
 
-<form action='virement.php' method='POST'>
-	<p>Choisir la personne à créditer :
-	<select name="clientACrediter">
-		<?php foreach ($clients as $unClient) { ?>
-		<option  value='<?php echo $unClient->getNumero(); ?>'><?php echo strtoupper($unClient->getNom()) ." ".$unClient->getPrenom(); ?></option>
-	<?php } ?>
-	</select> </p>
+		</div>
+			<br/>
+		<div class="row">
 
-<p>Choisir la personne à débiter : 
+			<div class="col-lg-4 col-md-4 col-xs-4">
+				<button type="submit" class="btn btn-danger">Valider virement</button>
+			</div>
 
-<form action='virement.php' method='POST'>
-	<select name="clientADebiter">
-		<?php foreach ($clients as $unClient) { ?>
-		<option  value='<?php echo $unClient->getNumero(); ?>'><?php echo strtoupper($unClient->getNom()) ." ".$unClient->getPrenom(); ?></option>
-	<?php } ?>
-	</select></p>
+		</div>
 
-<p>Choisir le montant</p>
+	</form>
+<?php
 
-	<input type="text" name="montant">
-	<input type='submit' value='Virer'>
-</form>
+	if(isset($_POST['somme']) and ($_POST['emetteur']) and ($_POST['destinataire']))
+	{
+		effectuerVirement($_POST['somme'], $_POST['emetteur'], $_POST['destinataire'], $clients);
+	}
 
+?>
 
-<?php 
-
-if (isset($_POST['montant']) AND isset($_POST['clientADebiter']) AND isset($_POST['clientACrediter'])) {
-	echo "<h1>Details du virement</h1>";
-	echo "montant de la transaction : ". $_POST['montant']. "<br>";
-	echo "Client débité : ". $clients[$_POST['clientADebiter']]->getNom(). " ". $clients[$_POST['clientADebiter']]->getPrenom() ."<br>";
-	echo "Client crédité : ". $clients[$_POST['clientACrediter']]->getNom(). " ". $clients[$_POST['clientACrediter']]->getPrenom() ."<br>";
+</div>
 
 
-	$clients[$_POST['clientADebiter']]->afficherInformations();
-	$clients[$_POST['clientADebiter']]->virement($clients[$_POST['clientACrediter']], $_POST['montant']);
-	$clients[$_POST['clientADebiter']]->afficherInformations();
-
-
-}
-
-include 'footer.php';
-
- ?>
-
+<?php include("footer.php");?>
