f58fa8143d82af3961cae894cfa20860940f2ff6
fearcraftor <flelong@live.fr>
2015-01-07 21:38:25 +0100

    Suggest
    Solution cool mais pas trop

--- listeClients.php
+++ listeClients.php
@@ -1,42 +1,53 @@
-<?php 
-include 'header.php';
- ?>
-  <h1>Voici la liste des clients  </h1>
+<?php include("header.php"); 
 
-<?php 
-	echo "<table class='table'>";
-    echo "<tr><th>Numero</th><th>Nom</th><th>Prenom</th><th>Adresse</th><th>Decouvert</th><th>Solde</th></tr>";
-         
-	foreach ($clients as $unClient) {
-                echo "<tr>";
-                
-                echo "<td>";
-                echo $unClient->getNumero();
-                echo "</td>";
-                
-                echo "<td>";
-                echo strtoupper($unClient->getNom());
-                echo "</td>";
+include("donneesTest.php");
 
-                echo "<td>";
-                echo $unClient->getPrenom();
-                echo "</td>";
+?>
 
-                echo "<td>";
-                echo $unClient->getAdresse();
-                echo "</td>";
+<div class="container">
 
-                echo "<td>";
-                echo $unClient->getCompte()->getDecouvert();
-                echo "</td>";                
+	<div class="page-header">
+	
+		<h1>Liste des clients</h1>
 
-                echo "<td>";
-                echo $unClient->getCompte()->getSolde() . "€";
-                echo "</td>";
-                               
-                echo "</tr>";
-            }
-  
-	echo "</table>";
+	</div>
 
- ?>
+	<table class="table table-bordered table-condensed table-striped">
+
+		<thead>
+			<tr>
+				<th>Nom</th>
+				<th>Prénom</th>
+				<th>Adresse</th>
+				<th>Solde</th>
+				<th>Découvert Autorisé</th>
+			</tr>
+		</thead>
+
+		<tbody>
+		<?php
+			$item1 = "nom";
+			$item2 = "prenom";
+			$item3 = "adresse";
+
+			foreach ($clients as $value)
+			{
+				$solde = $value -> getCompte() -> getSolde();
+				$autoDec = $value -> getCompte() -> getDec();
+				
+				echo "<tr>";
+				echo "<td>".$value -> getClient($item1)."</td>";
+				echo "<td>".$value -> getClient($item2)."</td>";
+				echo "<td>".$value -> getClient($item3)."</td>";
+				echo "<td>".number_format($solde, 2, ',', ' ')." €</td>";
+				echo "<td>".number_format($autoDec, 2, ',', ' ')." €</td>";
+				echo "</tr>";
+			}
+		?>
+		</tbody>
+
+	</table>
+
+</div>
+
+<?php include("footer.php");?>
